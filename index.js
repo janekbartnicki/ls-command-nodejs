8a4b659748bff355564d8faf7ac883e2d0b9a4ed
Jan Bartnicki <53710293+janekbartnicki@users.noreply.github.com>
2022-08-05 14:20:33 +0200

    1

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
 
 fs.readdir(process.cwd(), (err, files) => {
     if(err) throw new Error(err);
 
-    console.log(files)
-});
+    for(let file of files) {
+        fs.lstat(path.basename(file), (err, stat) => {
+            if(stat.isDirectory()) {
+                console.log(file, 'is directory');
+            }
+        })
+    }
+    })
