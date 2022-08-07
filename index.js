f5945b85a8c90810abaf74ad8df1491a72031da2
Jan Bartnicki <53710293+janekbartnicki@users.noreply.github.com>
2022-08-07 21:14:09 +0200

    1

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 import fsPromises from 'fs/promises';
 import chalk from 'chalk';
 
-fsPromises.readdir(process.cwd(), {withFileTypes: true}).then(files => {
+const locationArgument = process.argv[2] || process.cwd();
+
+fsPromises.readdir(locationArgument, {withFileTypes: true}).then(files => {
     for(let file of files) {
         console.log(file.isDirectory() ? chalk.blue(file.name) : file.name);
     }
-}).catch(err => console.log(err));
+}).catch(err => console.log(chalk.bgRedBright('ERROR'), err));
