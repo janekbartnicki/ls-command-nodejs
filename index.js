9a2839fdfb7b69637668d87bc1b4823540318137
Jan Bartnicki <53710293+janekbartnicki@users.noreply.github.com>
2022-08-07 12:51:00 +0200

    1

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const path = require('path');
+const fsPromises = require('fs/promises');
 
 fs.readdir(process.cwd(), (err, files) => {
     if(err) throw new Error(err);
 
+    // for(let file of files) {
+    //     fs.lstat(file, (err, stat) => {
+    //         if(err) throw new Error(err);
+    //         if(stat.isDirectory()) {
+    //             console.log('\x1b[36m%s\x1b[0m', file);             
+    //         } else console.log(file);
+    //     })
+    // }
+    const filesArray = [];
+
     for(let file of files) {
-        fs.lstat(path.basename(file), (err, stat) => {
+        fsPromises.lstat(file).then((stat) => {
+            if(err) console.log(err);
             if(stat.isDirectory()) {
-                console.log(file, 'is directory');
-            }
-        })
-    }
+                console.log('\x1b[36m%s\x1b[0m', file);             
+            } else console.log(file);
     })
+    }
+})
